7bf2cd47d8bc861ad5ccdfdecff6e2330a175656
Frumillon Pierre <contact@pistil-studio.fr>
2017-05-10 11:20:25 +0200

    gestion du title tag par wordpress

--- src/functions.php
+++ src/functions.php
@@ -1,5 +1,7 @@
 <?php
 
+add_theme_support( 'title-tag' );
+
 // REMOVE WP EMOJI
 remove_action('wp_head', 'print_emoji_detection_script', 7);
 remove_action('wp_print_styles', 'print_emoji_styles');
